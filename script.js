0fa844ced273713b30ccaaae4f60029c941abbf2
Lucas Barra <lucastbarra@hotmail.com>
2021-02-20 17:56:30 -0300

    js finalizado

--- script.js
+++ script.js
@@ -10,12 +10,41 @@ let inputDuracao = document.getElementById("duracao");
 
 let resultado = document.getElementById("resultado");
 
-function calcular(){
-    console.log("calculando...");
+function calcular() {
 
     let adultos = inputAdultos.value;
     let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
 
+    let qtdTotalCarne = (carnePorPessoa(duracao) * adultos) + (carnePorPessoa(duracao) / 2 * criancas);
+    let qtdTotalCerveja = cervejaPorPessoa(duracao) * adultos;
+    let qtdTotalBebidas = (bebidasPorPessoa(duracao) * adultos) + (bebidasPorPessoa(duracao) / 2 * criancas);
 
-    let qtdTotalCarne = (400 * adultos) + (400/2 * criancas); 
+    resultado.innerHTML = `<p>${qtdTotalCarne/1000}kg de carne</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja/355)} latas de cerveja</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebidas/1000)}L de água, refrigerante e suco</p>`
+}
+
+function carnePorPessoa(duracao) {
+    if (duracao >= 6) {
+        return 650
+    } else {
+        return 400
+    }
+}
+
+function cervejaPorPessoa(duracao) {
+    if (duracao >= 6) {
+        return 2000
+    } else {
+        return 1200
+    }
+}
+
+function bebidasPorPessoa(duracao) {
+    if (duracao >= 6) {
+        return 1500
+    } else {
+        return 1000
+    }
 }
